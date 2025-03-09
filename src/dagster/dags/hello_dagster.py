d67d3af5d36df11b3579110909a665146141fc47
Vincent Errick S Itucal <vsitucal@gmail.com>
2025-03-09 13:58:45 +0800

    more progress

--- src/dagster/dags/hello_dagster.py
+++ src/dagster/dags/hello_dagster.py
@@ -1,74 +1,95 @@
-from typing import List
+import matplotlib.pyplot as plt
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+import shap
+from evidently.metric_preset import ClassificationPreset
+from evidently.report import Report
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
-from dagster import (
-    AssetExecutionContext,
-    AssetIn,
-    AssetSelection,
-    Definitions,
-    ScheduleDefinition,
-    asset,
-    define_asset_job,
-)
+from dagster import Definitions, OpExecutionContext, asset, job
 
-
-@asset(key="my_awesome_first_asset", group_name="get_started")
-def my_first_asset(context: AssetExecutionContext):
-    """
-    This is our first asset for testing purposes
-    """
-    print("this is a print message.")
-    context.log.info("this is a log message.")
-    return [1, 2, 3]
+MLFLOW_TRACKING_URI = "http://mlflow:5000"
+mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
 
-@asset(
-    ins={"upstream": AssetIn(key="my_awesome_first_asset")},
-    group_name="get_started",
-)
-def my_second_asset(context: AssetExecutionContext, upstream: List):
-    """
-    This is our second asset
-    """
-    data = upstream + [4, 5, 6]
-    context.log.info(f"Output data is: {data}")
-    return data
+@asset
+def iris_dataset() -> pd.DataFrame:
+    iris = load_iris(as_frame=True)
+    df = iris.frame
+    df["target"] = iris.target
+    return df.head(20)
 
 
-@asset(
-    ins={
-        "first_upstream": AssetIn("my_awesome_first_asset"),
-        "second_upstream": AssetIn("my_second_asset"),
-    },
-    group_name="get_started",
-)
-def my_third_asset(
-    context: AssetExecutionContext, first_upstream: List, second_upstream: List
-):
-    """
-    This is our third asset
-    """
-    data = {
-        "first_asset": first_upstream,
-        "second_asset": second_upstream,
-        "third_asset": second_upstream + [7, 8],
-    }
-    context.log.info(f"Output data is: {data}")
-    return data
+@asset
+def split_data(iris_dataset: pd.DataFrame):
+    X = iris_dataset.drop(columns=["target"])
+    y = iris_dataset["target"]
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+    return {"X_train": X_train, "X_test": X_test, "y_train": y_train, "y_test": y_test}
+
+
+@asset
+def train_model(split_data):
+    X_train = split_data["X_train"]
+    y_train = split_data["y_train"]
+
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+
+    mlflow.sklearn.log_model(model, "iris_rf_model")
+    return model
+
+
+@asset
+def predict(train_model, split_data):
+    X_train = split_data["X_train"]
+    model = train_model
+
+    y_train_pred = model.predict(X_train)
+    return {"y_train_pred": y_train_pred}
+
+
+@asset
+def log_to_mlflow(context: OpExecutionContext, train_model, split_data, predict):
+    X_train = split_data["X_train"]
+    y_train = split_data["y_train"]
+    model = train_model
+
+    with mlflow.start_run():
+        mlflow.sklearn.log_model(model, "iris_rf_model")
+        mlflow.log_params({"n_estimators": 100, "random_state": 42})
+        mlflow.log_metric("train_accuracy", model.score(X_train, y_train))
+
+        explainer = shap.TreeExplainer(model)
+        shap_values = explainer.shap_values(X_train)
+        shap.summary_plot(shap_values, X_train, show=False)
+        plt.savefig("shap_summary.png")
+        mlflow.log_artifact("shap_summary.png")
+
+        report = Report(metrics=[ClassificationPreset()])
+        report.run(
+            reference_data=pd.concat([X_train, y_train], axis=1),
+            current_data=pd.concat([X_train, y_train], axis=1),
+        )
+        report.save_html("evidently_report.html")
+        mlflow.log_artifact("evidently_report.html")
+
+
+@job
+def iris_training_job():
+    dataset = iris_dataset()
+    split = split_data(dataset)
+    model = train_model(split)
+    predictions = predict(model, split)
+    log_to_mlflow(model, split, predictions)
 
 
 defs = Definitions(
-    assets=[my_first_asset, my_second_asset, my_third_asset],
-    jobs=[
-        define_asset_job(
-            name="hello_dagster_job",
-            selection=AssetSelection.groups("get_started"),
-        )
-    ],
-    schedules=[
-        ScheduleDefinition(
-            name="hello_dagster_schedule",
-            job_name="hello_dagster_job",
-            cron_schedule="* * * * *",
-        )
-    ],
+    assets=[iris_dataset, split_data, train_model, predict],
+    jobs=[iris_training_job],
 )
